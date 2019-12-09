f1ca661ce820daa7ca632461e2f28caae7143cad
Luke Ginger <rc0039889@student.riversidecollege.ac.uk>
2019-12-09 15:20:46 +0000

    ff

--- Trgonometry/Form1.cs
+++ Trgonometry/Form1.cs
@@ -16,5 +16,33 @@ namespace Trgonometry
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void PictureBox3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void PictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            //input values for Force and Angle from textboxes
+            double Force = double.Parse(textBox1.Text);
+            double Angle = double.Parse(textBox2.Text);
+            
+            //calculate values for Fx and Fy
+            Double Fx = Force* cos(Angle);
+            Double Fy = Force* sin(Angle);
+
+
+        }
     }
 }
